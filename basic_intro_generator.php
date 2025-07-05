dcc39479226a53932cd34367a57a0237040d78ed
John Vincent Pangilinan <johnvincentppangilinan@gmail.com>
2025-07-05 23:26:23 +0800

    Add files via upload

---
+++ basic_intro_generator.php
@@ -0,0 +1,59 @@
+<?php
+
+//Testing topic:PHP Module 1: PHP Syntax, Echo, Comments, Variables and Data Types
+//$x=22;
+//$y= "String";
+/*$float = 99.99;
+
+echo "This is a data type" .$x . "<br>";
+echo  $y ."<br>";
+echo "This float is " .$float . "%" . "<br>";*/
+
+
+
+//basic_intro_generator.php:
+/*
+$name = "John Vincent P. Pangilinan";
+$age = 22;
+$hometown = "Binan, Laguna";
+$course = "BS Information Technology";
+$IsGraduating = true ? "True" : "False";
+
+echo "Hi!! My name is " . $name .".". "<br>";
+echo "I am " . $age . "Years old from " . $hometown ."<br>";
+echo "And Yes I am an " . $course. "and soon to be  graduating  " .$IsGraduating ."<br>";*/
+
+
+
+
+//profile_data_check.php:
+/*$name = "John Vincent P. Pangilinan";
+$age = 22;
+$GPA =1.49;
+$IsWorkingStudent = false? "True": "False";
+
+var_dump($name);
+var_dump($age);
+var_dump($GPA);
+var_dump($IsWorkingStudent);*/
+
+
+
+
+//summary_report_generator.php
+$name ="John Vincent P. Pangilinan";
+$age = 22;
+$birthday = "February 8, 2003";
+$address = "Brgy. Timbao, Binan, Laguna";
+$status ="Graduating Student";
+$course ="Bachelor of Science in Information Technology";
+$GWA= 1.49;
+
+echo "Name: ". $name ."<br>";
+echo "Age: ". $age .  "<br>";
+echo "Birthday: " . $birthday . "<br>";
+echo "Address: " . $address . "<br>";
+echo "Course: " .$course. "<br>";
+echo "GWA: ". $GWA. "%". "<br>";
+
+?>
